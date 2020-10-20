6c43568ffb4817b1689b92cd17873436cdf5c740
hjs6877 <hjs6877@example.com>
2020-10-20 23:20:23 +0900

    Modify BlockingGet and BlockingSingle

--- src/test/java/com/itvillage/section03/BlockingSingleTest.java
+++ src/test/java/com/itvillage/section03/BlockingSingleTest.java
@@ -8,16 +8,25 @@ import static org.hamcrest.MatcherAssert.assertThat;
 /**
  * blockingSingle을 사용한 통지된 첫번째 데이터를 테스트하는 예제
  */
-public class BlockingSingleTest extends RxJavaTest {
+public class BlockingSingleTest {
 
     // A 지점의 월간 매출 중에 30,000,000원 이상인 매출액의 첫번째 데이터를 테스트
     @Test
     public void totalSalesOfBranchATest(){
-        int actual = sampleObservable.getSalesOfBranchA()
+        int actual = SampleObservable.getSalesOfBranchA()
                             .filter(sale -> sale > 30_000_000)
                             .take(1)
                             .blockingSingle();
 
         assertThat(actual, is(35_000_000));
+    }
+
+    // A 지점의 월간 매출 중에 30,000,000원 이상인 매출액의 첫번째 데이터를 테스트
+    @Test(expected = IllegalArgumentException.class)
+    public void totalSalesOfBranchATest2(){
+        SampleObservable.getSalesOfBranchA()
+                .filter(sale -> sale > 30_000_000)
+                .take(2)
+                .blockingSingle();
     }
 }
